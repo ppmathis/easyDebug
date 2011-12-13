febf2e4c69f492df5ecfa6fb8bf8ee3042cb2f51
Pascal Mathis <github@snapserv.net>
2011-12-13 20:20:44 +0100

    easyDebug v1.1.4 beta - As requested, it is now a fixed floating element (MS limitation: >IE6 required, other browsers are working normally)

--- example.php
+++ example.php
@@ -44,3 +44,82 @@
 	// [EXAMPLE] End of html file
 	echo '</body></html>';
 ?>
+<pre>
+EnableExplicit
+
+Define windowID, editorGadget, windowEvent, oldCallback, detectedCharacterLimit, detectedLineLimit
+
+; Custom window callback
+Procedure callbackHandler(hWnd, uMsg, wParam, lParam)
+  Shared oldCallback, detectedCharacterLimit, detectedLineLimit, editorGadget
+  Protected scrollStyle, actualStyle, lineCount, charCount
+  
+  ; Disable all scrollbars
+  Select uMsg
+    Case #WM_SIZE
+      ; Remove all settings, they're now invalid
+      detectedCharacterLimit = 0
+      detectedLineLimit = 0
+      SendMessage_(hWnd, #EM_SETLIMITTEXT, 0, 0)
+    Case #WM_KEYDOWN
+      If wParam = #VK_RETURN
+        ; Get line count
+        lineCount = SendMessage_(hWnd, #EM_GETLINECOUNT, 0, 0)
+        
+        ; Text already long enough?
+        If detectedLineLimit > 0 And lineCount = detectedLineLimit
+          ; Block ENTER
+          ProcedureReturn 0
+        EndIf
+      EndIf
+    Case #WM_NCCALCSIZE
+      scrollStyle = #WS_VSCROLL | #WS_HSCROLL
+      actualStyle = GetWindowLong_(hWnd, #GWL_STYLE) 
+
+      If (actualStyle & scrollStyle) <> 0
+        ; Get char count
+        lineCount = SendMessage_(hWnd, #EM_GETLINECOUNT, 0, 0)
+        charCount = Len(GetGadgetText(editorGadget))
+        
+        ; Detect allowed chars
+        detectedCharacterLimit = charCount - 1
+        detectedLineLimit = lineCount - 1
+        SendMessage_(hWnd, #EM_SETLIMITTEXT, detectedCharacterLimit, 0)
+        RemoveGadgetItem(editorGadget, lineCount - 1)
+        SendMessage_(hWnd, #EM_SCROLL, #SB_PAGEUP, 0)
+        
+        ; Disable scrollbars always
+        SetWindowLong_(hWnd, #GWL_STYLE, actualStyle & (~scrollStyle))
+      EndIf
+  EndSelect
+  
+  ; Continue with normal window events
+  ProcedureReturn CallWindowProc_(oldCallback, hWnd, uMsg, wParam, lParam)
+EndProcedure
+
+; Create window & check for success
+windowID = OpenWindow(#PB_Any, 0, 0, 200, 200, "[EditorGadget] Line limit", #PB_Window_SystemMenu | #PB_Window_ScreenCentered)
+If windowID
+  ; Create editor gadget with custom callback
+  editorGadget = EditorGadget(#PB_Any, 0, 0, 200, 200)
+  SendMessage_(GadgetID(editorGadget), #EM_SETTARGETDEVICE, #Null, 0)
+  oldCallback = SetWindowLong_(GadgetID(editorGadget), #GWL_WNDPROC, @callbackHandler())
+  SetActiveGadget(editorGadget)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
+  While detectedCharacterLimit = 0
+                                                                                                                                                                                                                                                                                                                                                                          WindowEvent()                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
+    keybd_event_(#VK_A, 0, 0, 0)
+    keybd_event_(#VK_A, 0, #KEYEVENTF_KEYUP, 0)
+  Wend
+  SetGadgetText(editorGadget, "")                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+  
+  ; Window event loop
+  Repeat
+    windowEvent = WaitWindowEvent()
+  Until windowEvent = #PB_Event_CloseWindow
+EndIf
+; IDE Options = PureBasic 4.60 RC 2 (Windows - x64)
+; CursorPosition = 62
+; FirstLine = 14
+; Folding = -
+; EnableXP
+</pre>
